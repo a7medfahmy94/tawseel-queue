0a31673e39846cb399d9bb44b9783f0e58cb3e50
Ahmed Fahmy <a7med.fahmy94@gmail.com>
2016-12-06 21:56:16 +0200

    firebase queue job listening on /queue/tasks

--- index.js
+++ index.js
@@ -1,105 +1,65 @@
 // Requires
-var Firebase = require("firebase");
-var Queue = require("firebase-queue");
-var bodyParser = require('body-parser')
 var express = require('express');
+var firebase = require('firebase');
+var Queue = require('firebase-queue');
 // End requires
 
 // Init express
 var app = express();
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-/** bodyParser.urlencoded(options)
- * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
- * and exposes the resulting object (containing the keys and values) on req.body
- */
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-/**bodyParser.json(options)
- * Parses the text as JSON and exposes the resulting object on req.body.
- */
-app.use(bodyParser.json());
-// End init express
+app.set('port', 7000);
 
+app.listen(7000);
 
-// Init firebase
-var initFirebase = function() {
-  var config = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.PROJECT_ID + ".firebaseapp.com",
-    databaseURL: "https://" + process.env.DATABASE_NAME + ".firebaseio.com",
-    storageBucket: process.env.BUCKET + ".appspot.com",
-    messagingSenderId: process.env.MESSAGING_SENDER_ID
-  };
-  Firebase.initializeApp(config);
-}
-initFirebase();
-// End init firebase
+var config = {
+  apiKey: 'AIzaSyDn_NMUsa9sVsYm19ApwK2U8juSC1PYLfM',
+  authDomain: "tawseel-37034.firebaseapp.com",
+  databaseURL: "https://tawseel-37034.firebaseio.com",
+  storageBucket: "tawseel-37034.appspot.com",
+  messagingSenderId: "534910965048"
+};
+firebase.initializeApp(config);
 
+var database = firebase.database();
+var queueRef = database.ref('/queue');
 
-// Firebase queue
-//Database ref
-var Database = Firebase.database();
-var tripsRef = Database.ref('/trips');
-
-// var queue = new Queue(queueRef, function (data, progress, resolve, reject) {
-//   console.log(data);
-//   // Database.ref(data.ref)
-//   resolve();
-// })
-// End firebase queue
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.post('/move', function(request, response) {
-  var body = request.body;
-  var tripRef = tripsRef.child(body.tripId);
-  tripRef.once('value').then(function(tripSnapshot){
-    var trip = tripSnapshot.val();
-    if (!trip.points) {
-      trip.points = [];
+var queue = new Queue(queueRef, function (data, progress, resolve, reject) {
+  database.ref('/trips/' + data.tripId).once('value', function(snapshot){
+    var trip = snapshot.val();
+    console.log(trip.logs);
+    var distanceKM = 0;
+    var sarPerKM = 10;
+    var points = []
+    for (var key in trip.logs) {
+      var log = trip.logs[key];
+      points.push(log.point);
     }
-    trip.points.push(body.point);
-    var cost = Math.round((body.cost/1000.0) * 10 * 100) / 100
-    trip.cost = cost;
-    tripRef.update(trip);
-    response.json({cost: cost});
+    for (var i = 1; i < points.length; i++) {
+      var p1 = points[i-1];
+      var p2 = points[i];
+      distanceKM += getDistanceFromLatLonInKm(p1.lat, p1.lng, p2.lat, p2.lng);
+      console.log(distanceKM);
+    }
+    var totalTripCost = (sarPerKM * distanceKM).toFixed(2);
+    database.ref('/trips/' + data.tripId + '/cost').set(totalTripCost);
+    resolve();
   })
-});
-
-app.post('/end', function (request, response) {
-  console.log('end');
-  var tripRef = tripsRef.child(request.body.tripId);
-  tripRef.update({state: 'ended'});
 })
 
-app.get('/newTrip', function(request, response){
-  var key = tripsRef.push().key;
-  tripsRef.child(key).update({
-    startAt: new Date(),
-    state: 'started'
-  })
-  response.json({tripId: key});
-});
+// see http://stackoverflow.com/a/27943/4077057
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+  return d;
+}
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
-//Begin
-//
-//
-//
-//
-// queueRef.child('tasks').push({
-//   startPoint: 10,
-//   endPoint: 20,
-//   tripId: 1
-// })
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
